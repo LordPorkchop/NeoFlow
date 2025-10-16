be145889fc11c784c315c386eb8eb03ea667357e
Lord Porkchop <149256533+LordPorkchop@users.noreply.github.com>
2025-10-17 01:49:28 +0200

    Fixed Error
    Removed the call of a deleted file

--- run.py
+++ run.py
@@ -4,6 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    os.system("python update.py")
     app.run(host="0.0.0.0", port=5000, debug=True,
             ssl_context=('./app/certificates/cert.pem', './app/certificates/key.pem'))
