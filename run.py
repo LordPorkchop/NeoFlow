b9726d48588c73b46fe3aadfd6d60850cee1eae8
Lord Porkchop <149256533+LordPorkchop@users.noreply.github.com>
2025-10-17 13:45:20 +0200

    Moved host
    Moved host from all adresses to localhost
    Hid .vscode directory

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     app.logger.info("Server started")
 
     try:
-        app.run(host="0.0.0.0", port=5000, debug=True,
+        app.run(host="127.0.0.1", port=5000, debug=True,
                 ssl_context=('./app/certificates/cert.pem', './app/certificates/key.pem'))
     except KeyboardInterrupt:
         app.logger.info("Server stopped after user termination")
